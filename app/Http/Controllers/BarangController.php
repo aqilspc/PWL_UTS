7533c55860ab6bdef3ece1fa7b1fff32ecff1a7a
blackcode@DESKTOP-CC0RJLG <aqilblackcode@gmail.com>
2021-03-30 18:16:08 +0700

    query search

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -16,30 +16,17 @@ class BarangController extends Controller
     {
     	$object = $request->cari;
     	$barang='';
-    	$katgori = Barang::where('kategori_barang',$object)->get();
-    	if(!$katgori->isEmpty())
+    	$search = Barang::where('kategori_barang',$object)
+    					->orWhere('nama_barang',$object)
+    					->orWhere('kode_barang',$object)
+    					->get();
+    	if(!$search->isEmpty())
     	{
-    		$barang = $katgori;
+    		$barang = $search;
     		return view('hasil',compact('barang','object'));
     	}else
     	{
-    		$nama = Barang::where('nama_barang',$object)->first();
-    		if($nama)
-    		{
-    			$barang = $nama;
-    			return view('hasil_nama',compact('barang','object'));
-    		}else
-    		{
-    			$kode = Barang::where('kode_barang',$object)->first();
-    			if($kode)
-    			{
-    				$barang = $kode;
-    				return view('hasil_kode',compact('barang','object'));
-    			}else
-    			{
-    				return 'barang '.$object.' yang anda cari tidak ada! <a class="btn btn-success mt-3" href="'.url('/').'">Home</a>';
-    			}
-    		}
+    		return 'barang '.$object.' yang anda cari tidak ada! <a class="btn btn-success mt-3" href="'.url('/').'">Home</a>';
     	}
     }
 
